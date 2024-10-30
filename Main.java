1c67e520318d66a7f88a8efea22bc58ec81dcd93
Alejandro <alejandrogalindo9411@gmail.com>
2024-10-30 12:54:14 +0100

    Se ha creado la rama del programador1

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ public class main{
     //Este metodo lo va a crear el programador1
     //Este metodo recibe un numero por parametro e  indica si es par o  impar
 
+    //El programador 1 ha llegado
+
 
 
     //Este metodo lo va a crear el programador2
