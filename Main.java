7561bace75f76699fb6c80dd01ec4c521b434c18
Alejandro <alejandrogalindo9411@gmail.com>
2024-10-30 12:34:22 +0100

    Se a creado la clase main

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class main{
+    //Este metodo lo va a crear el programador1
+    //Este metodo recibe un numero por parametro e  indica si es par o  impar
+
+
+
+    //Este metodo lo va a crear el programador2
+    //Este metodo  recibe un numero  y comprobara si es primo.
+}
